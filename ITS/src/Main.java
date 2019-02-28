db75ab506b4e83410e2bc19a270c0d2440b4ebe6
Danielle <smeltzerde1@gcc.edu>
2019-02-28 10:17:12 -0500

    Basic buttons

--- ITS/src/Main.java
+++ ITS/src/Main.java
@@ -12,6 +12,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -19,19 +20,20 @@ import javafx.stage.Stage;
 import javafx. *;
 
 
-public class Main /*extends Application*/{
+public class Main extends Application{
 
 	public static void main(String[] args) {
-        //launch(args);
+        launch(args);
 		
+		/*
 		System.out.println("Hello World!");
 		System.out.println("I hate everything.");
 		System.out.println("This is way harder than it needs to be.");
-		System.out.println("Goodbye World!");
+		System.out.println("Goodbye World!"); */
 		
     }
     
-    /*@Override
+    @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("AP Computer Science Prep");
         
@@ -50,15 +52,91 @@ public class Main /*extends Application*/{
         Scene scene = new Scene(grid, 1080, 720);
         primaryStage.setScene(scene);
         
-        
         //Layout Text for welcome screen
         Text scenetitle = new Text("Welcome Back!");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 26));
         grid.add(scenetitle, 12, 3, 2, 1);
         
+        
+        //
+        //NAVIGATION BAR
+        //Maybe copy this code for the other pages???
+        //
+        //home button
+        Button home = new Button();
+        home.setText("Home");
+        home.setTextFill(Color.BLUE);
+        home.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("Pressed Home Button");
+        	}
+        });
+        grid.add(home, 5, 3,2,1);
+        
+        //textbook button
+        Button textbk = new Button();
+        textbk.setText("Textbook");
+        textbk.setTextFill(Color.BLUE);
+        textbk.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("Pressed Textbook Button");
+        	}
+        });
+        grid.add(textbk, 4, 10,2,1);
+        
+        //lesson button
+        Button lessons = new Button();
+        lessons.setText("Lessons");
+        lessons.setTextFill(Color.BLUE);
+        lessons.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("Pressed Lessons Button");
+        	}
+        });
+        grid.add(lessons, 5, 18,2,1);
+        
+        //Test/quizzes button
+        Button exams = new Button();
+        exams.setText("Quizzes and Exams");
+        exams.setTextFill(Color.BLUE);
+        exams.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("Pressed Exams Button");
+        	}
+        });
+        grid.add(exams, 3, 25,5,1);
+        
+        //diagnostics button
+        Button diagnostics = new Button();
+        diagnostics.setText("Diagnostics");
+        diagnostics.setTextFill(Color.BLUE);
+        diagnostics.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("Pressed Diagnostics Button");
+        	}
+        });
+        grid.add(diagnostics, 5, 33 ,5,1);
+        
+        //settings button
+        Button settings = new Button();
+        settings.setText("Settings");
+        settings.setTextFill(Color.BLUE);
+        settings.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("Pressed Settings Button");
+        	}
+        });
+        grid.add(settings, 5, 40,5,1);
+        
         //add style sheet
-        scene.getStylesheets().add
-        (Main.class.getResource("StyleForCAI.css").toExternalForm());
+        //scene.getStylesheets().add
+        //(Main.class.getResource("StyleForCAI.css").toExternalForm());
         primaryStage.show();
-    }*/
+    }
 }
