7a1ca8b66716c1cedd38764fb892dd2b8dd3cc6b
Danielle <smeltzerde1@gcc.edu>
2019-03-01 10:56:13 -0500

    Added switching between the home button and the textbook button

--- ITS/src/Main.java
+++ ITS/src/Main.java
@@ -1,5 +1,5 @@
-//import java.awt.Insets;
-//import java.geometry;
+
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -23,6 +23,7 @@ import javafx. *;
 public class Main extends Application{
 
 	public static void main(String[] args) {
+		//GridPane grid = new GridPane();
         launch(args);
 		
 		/*
@@ -36,10 +37,10 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("AP Computer Science Prep");
-        
+        GridPane grid = new GridPane();
 
         //creates a grid in the center
-        GridPane grid = new GridPane();
+        //GridPane grid = new GridPane();
         grid.setAlignment(Pos.TOP_LEFT);
         
         grid.setHgap(10);
@@ -51,92 +52,107 @@ public class Main extends Application{
         //makes size of the window
         Scene scene = new Scene(grid, 1080, 720);
         primaryStage.setScene(scene);
-        
-        //Layout Text for welcome screen
+        //adds preliminary home screen UI
         Text scenetitle = new Text("Welcome Back!");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 26));
         grid.add(scenetitle, 12, 3, 2, 1);
-        
-        
-        //
-        //NAVIGATION BAR
-        //Maybe copy this code for the other pages???
-        //
-        //home button
-        Button home = new Button();
-        home.setText("Home");
-        home.setTextFill(Color.BLUE);
-        home.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent event) {
-        		System.out.println("Pressed Home Button");
-        	}
-        });
-        grid.add(home, 5, 3,2,1);
-        
-        //textbook button
-        Button textbk = new Button();
-        textbk.setText("Textbook");
-        textbk.setTextFill(Color.BLUE);
-        textbk.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent event) {
-        		System.out.println("Pressed Textbook Button");
-        	}
-        });
-        grid.add(textbk, 4, 10,2,1);
-        
-        //lesson button
-        Button lessons = new Button();
-        lessons.setText("Lessons");
-        lessons.setTextFill(Color.BLUE);
-        lessons.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent event) {
-        		System.out.println("Pressed Lessons Button");
-        	}
-        });
-        grid.add(lessons, 5, 18,2,1);
-        
-        //Test/quizzes button
-        Button exams = new Button();
-        exams.setText("Quizzes and Exams");
-        exams.setTextFill(Color.BLUE);
-        exams.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent event) {
-        		System.out.println("Pressed Exams Button");
-        	}
-        });
-        grid.add(exams, 3, 25,5,1);
-        
-        //diagnostics button
-        Button diagnostics = new Button();
-        diagnostics.setText("Diagnostics");
-        diagnostics.setTextFill(Color.BLUE);
-        diagnostics.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent event) {
-        		System.out.println("Pressed Diagnostics Button");
-        	}
-        });
-        grid.add(diagnostics, 5, 33 ,5,1);
-        
-        //settings button
-        Button settings = new Button();
-        settings.setText("Settings");
-        settings.setTextFill(Color.BLUE);
-        settings.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent event) {
-        		System.out.println("Pressed Settings Button");
-        	}
-        });
-        grid.add(settings, 5, 40,5,1);
-        
-        //add style sheet
-        //scene.getStylesheets().add
-        //(Main.class.getResource("StyleForCAI.css").toExternalForm());
-        primaryStage.show();
+        buttons(grid, primaryStage);
     }
-}
+        
+    
+        public void buttons(GridPane grid, Stage primaryStage){
+	        //
+	        //NAVIGATION BAR
+	        //Maybe copy this code for the other pages???
+	        //
+	        //home button
+	        Button home = new Button();
+	        home.setText("Home");
+	        home.setTextFill(Color.BLUE);
+	        home.setOnAction(new EventHandler<ActionEvent>() {
+	        	@Override
+	        	public void handle(ActionEvent event) {
+	        		System.out.println("Pressed Home Button");
+	        		// clear current screen
+	        		grid.getChildren().clear(); 
+	        		//add home screen shenanigans
+	                Text scenetitle = new Text("Welcome Back!");
+	                scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 26));
+	                grid.add(scenetitle, 12, 3, 2, 1);
+	        		//calls buttons to set up the navbar again
+	        		buttons(grid, primaryStage);
+	                System.out.println("finished creating");
+	        	}
+	        });
+	        grid.add(home, 5, 3,2,1);
+	        
+	        //textbook button
+	        Button textbk = new Button();
+	        textbk.setText("Textbook");
+	        textbk.setTextFill(Color.BLUE);
+	        textbk.setOnAction(new EventHandler<ActionEvent>() {
+	        	@Override
+	        	public void handle(ActionEvent event) {
+	        		System.out.println("Pressed Textbook Button");
+	        		grid.getChildren().clear();
+	        		buttons(grid, primaryStage);
+	        		System.out.println("finished creating");
+	        	}
+	        });
+	        grid.add(textbk, 4, 10,2,1);
+	        
+	        //lesson button
+	        Button lessons = new Button();
+	        lessons.setText("Lessons");
+	        lessons.setTextFill(Color.BLUE);
+	        lessons.setOnAction(new EventHandler<ActionEvent>() {
+	        	@Override
+	        	public void handle(ActionEvent event) {
+	        		System.out.println("Pressed Lessons Button");
+	        	}
+	        });
+	        grid.add(lessons, 5, 18,2,1);
+	        
+	        //Test/quizzes button
+	        Button exams = new Button();
+	        exams.setText("Quizzes and Exams");
+	        exams.setTextFill(Color.BLUE);
+	        exams.setOnAction(new EventHandler<ActionEvent>() {
+	        	@Override
+	        	public void handle(ActionEvent event) {
+	        		System.out.println("Pressed Exams Button");
+	        	}
+	        });
+	        grid.add(exams, 3, 25,5,1);
+	        
+	        //diagnostics button
+	        Button diagnostics = new Button();
+	        diagnostics.setText("Diagnostics");
+	        diagnostics.setTextFill(Color.BLUE);
+	        diagnostics.setOnAction(new EventHandler<ActionEvent>() {
+	        	@Override
+	        	public void handle(ActionEvent event) {
+	        		System.out.println("Pressed Diagnostics Button");
+	        	}
+	        });
+	        grid.add(diagnostics, 5, 33 ,5,1);
+	        
+	        //settings button
+	        Button settings = new Button();
+	        settings.setText("Settings");
+	        settings.setTextFill(Color.BLUE);
+	        settings.setOnAction(new EventHandler<ActionEvent>() {
+	        	@Override
+	        	public void handle(ActionEvent event) {
+	        		System.out.println("Pressed Settings Button");
+	        	}
+	        });
+	        grid.add(settings, 5, 40,5,1);
+	        
+	        //add style sheet
+	        //scene.getStylesheets().add
+	        //(Main.class.getResource("StyleForCAI.css").toExternalForm());
+	        primaryStage.show();
+        }
+    }
+
