b67f09b6bc32cfadefe24a21ccd5456c34b66672
Danielle <smeltzerde1@gcc.edu>
2019-03-19 19:23:05 -0400

    added a faux question

--- ITS/src/Main.java
+++ ITS/src/Main.java
@@ -5,11 +5,13 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -27,9 +29,8 @@ public class Main extends Application{
 	public static void main(String[] args) {
 		//GridPane grid = new GridPane();
 		//TESTY TESTY
-				
-			
-				
+		//create bank
+		
 				
         launch(args);
 		
@@ -194,41 +195,7 @@ public class Main extends Application{
 	        		System.out.println("finished creating");
 	        		
 
-	        		//create bank
-	        		TestBank banky = new TestBank();
-					ArrayList<String> answers = new ArrayList<String>();
-					answers.add("x = 4");
-					answers.add("x = 87");
-					answers.add("x = -2");
-					answers.add("x = 34");
-					
-					Question q1 = new Question("What is 2 + 2?", answers, "Arithmetic", "Analyze");
-
-					answers.set(0, "An integer");
-					answers.set(1, "A word");
-					answers.set(2, "A character");
-					answers.set(3, "A data structure");
-
-					Question q2 = new Question("What is an int?", answers, "Data Types", "Remember");
-					
-					answers.set(0, "A thing (right)");
-					answers.set(1, "Something else");
-					answers.set(2, "Some other thing");
-					answers.set(3, "Something else ALTOGETHER");
-					
-					Question q3 = new Question("What does \'static\' mean?", answers, "Arithmetic", "Remember");
-					
-					answers.set(0, "An integer");
-					answers.set(1, "A word");
-					answers.set(2, "A character");
-					answers.set(3, "A data structure");
-
-					Question q4 = new Question("What is a char?", answers, "Data Types", "Remember");
-					
-					banky.addQuest(q1);
-					banky.addQuest(q2);
-					banky.addQuest(q3);
-					banky.addQuest(q4);
+	        		
 	        		
 					//add button here 
 			        Button takeQuizButt = new Button();
@@ -238,18 +205,35 @@ public class Main extends Application{
 			        takeQuizButt.setOnAction(new EventHandler<ActionEvent>() {
 			        	@Override
 			        	public void handle(ActionEvent event) {
-			        
-	        		ArrayList<String> topics = new ArrayList<String>();
-	        		topics.add("Arithmetic");
-	        		topics.add("Data Types");
-	        		Testing quiz = new Testing(1, topics, banky);
-	        		
-	        		for(int i = 0; i < 4; i++){
-	        			System.out.println(quiz.getQ(i).getText());
-	        			for(int j = 0; j < 4; j++){
-	        				System.out.println("\t"+quiz.getQ(i).getAnswer(j));
-	        			}
-	        		}
+			        		
+			        		grid.getChildren().remove(takeQuizButt);
+			        		//add questions
+			                // create a tile pane 
+			                TilePane quest = new TilePane(); 
+			          
+			                // create a label 
+			                //this will become the question
+			                Label l = new Label("An object is an instance of a ________. "); 
+			          
+			                // string array
+			                //this will become the possible answers
+			                String st[] = { "method", "object", "class", "interface" }; 
+			          
+			                // add label 
+			                quest.getChildren().add(l); 
+			          
+			                for (int i = 0; i < st.length; i++) { 
+			          
+			                    // create a checkbox 
+			                    CheckBox c = new CheckBox(st[i]); 
+			          
+			                    // add label 
+			                    quest.getChildren().add(c); 
+			          
+			                    // set IndeterMinate 
+			                    c.setIndeterminate(true); 
+			                } 
+			                grid.add(quest, 3, 8);
 			        }
 			        });
 			        grid.add(takeQuizButt, 10, 18);
